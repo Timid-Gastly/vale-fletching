9655456d2948b8f22995c326b8bfa075da23d85b
Timid Gastly <zyounger@alumni.unc.edu>
2025-07-23 16:29:57 -0400

    Initial commit with properly named files and a slim README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Example
-An example greeter plugin
+# Vale Fletching
+
+WIP
+
+Plugin to provide highlights and information related to the Auburnvale Fletching activity.
+
+(note: this is my first plugin, please be kind.. or don't ¯\_(ツ)_/¯)
--- src/main/java/com/valefletching/ValeFletchingPlugin.java
+++ src/main/java/com/valefletching/ValeFletchingPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.valefletching;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Vale Fletching"
 )
-public class ExamplePlugin extends Plugin
+public class ValeFletchingPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ValeFletchingConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ValeFletchingConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ValeFletchingConfig.class);
 	}
 }
